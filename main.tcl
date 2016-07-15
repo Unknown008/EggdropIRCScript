be18c3f157a22699a10da892ffc26aaf0b60b196
Jerry <jerryysk.stillwaters@gmail.com>
2016-07-15 14:06:38 +0400

    Update main.tcl

--- main.tcl
+++ main.tcl
@@ -64,25 +64,25 @@ proc poke:challenge {nick host hand chan arg} {
   } else {
     putquick "PRIVMSG $chan :$nick issued a challenge against $target!"
     putquick "NOTICE $target :Type \"accept\" to accept the challenge or \"decline\" to decline."
-    bind pub - accept poke:decline
+    bind pub - decline poke:decline
     lappend poke(prepList) $target
   }
 }
 
 proc poke:accept {nick host hand chan arg} {
   global poke
-  if {!$poke(running) || $chan ne $poke(chan)} {return}
+  if {!$poke(running) || $chan ne $poke(chan) || $nick eq [lindex $poke(prepList) 0]} {return}
   set trainerCount [llength $poke(prepList)]
-  if {$trainerCount == 2 && $nick ne [lindex $poke(prepList) 1]} {return}
+  if {$trainerCount == 2 && [string trim $nick] ne [lindex $poke(prepList) 1]} {return}
   unbind pub - accept poke:accept
   lassign $poke(prepList) challenger target
   if {$target eq ""} {
     set target $nick
   }
-  putquick "PRIVMSG $chan :Battle between the challenger $nick and trainer $target is about to begin!"
+  putquick "PRIVMSG $chan :Battle between the challenger $challenger and trainer $target is about to begin!"
   putquick "PRIVMSG $challenger :Tell me your team details (you can link pastebin.com). Say \"help\" if you don't know the syntax."
   putquick "PRIVMSG $target :Tell me your team details (you can link pastebin.com). Say \"help\" if you don't know the syntax."
-  bind msgm - *!*@* poke:battleprep
+  bind msgm - "*" poke:battleprep
 }
 
 proc poke:decline {nick host hand chan arg} {
@@ -106,11 +106,12 @@ proc poke:stop {nick host hand chan arg} {
   set poke(crules)       6
   set poke(team)         [list]
   set poke(currentPoke)  [list]
-  unbind msgm - *!*@* poke:battleprep
+  unbind msgm - "*" poke:battleprep
 }
 
 proc poke:battleprep {nick host hand arg} {
   global poke
+  putlog "$nick said $arg"
   if {$nick ni $poke(prepList)} {return}
   if {$nick in $poke(forfeit)} {
     switch -nocase -regexp -- $arg {
@@ -134,11 +135,11 @@ proc poke:battleprep {nick host hand arg} {
     switch -nocase -glob -- $arg {
       help {
         putquick "PRIVMSG $nick :See this pastebin for the syntax: http://pastebin.com/Ym1amdKE"
-        putquick "PRIVMSG $nick :Use "forfeit" to give up."
-        putquick "PRIVMSG $nick :Use "cancel #" to remove the Pokemon in the #th slot."
-        putquick "PRIVMSG $nick :Use "reorder # # # # # # > # # # # # #" to reorder your Pokemon order."
-        putquick "PRIVMSG $nick :Use "done" to finalize and submit your team."
-        putquick "PRIVMSG $nick :Use "pastebin linktopastebin" to submit a team from pastebin (it should also follow the required format)"        
+        putquick "PRIVMSG $nick :Use \"forfeit\" to give up."
+        putquick "PRIVMSG $nick :Use \"cancel #\" to remove the Pokemon in the #th slot."
+        putquick "PRIVMSG $nick :Use \"reorder # # # # # # > # # # # # #\" to reorder your Pokemon order."
+        putquick "PRIVMSG $nick :Use \"done\" to finalize and submit your team."
+        putquick "PRIVMSG $nick :Use \"pastebin linktopastebin\" to submit a team from pastebin (it should also follow the required format)"        
       }
       forfeit {
         putquick "PRIVMSG $nick :Are you sure you want to forfeit? (Y/N)"
@@ -149,7 +150,7 @@ proc poke:battleprep {nick host hand arg} {
         if {$param > 0 && $param <= $poke(crule)} {
           
         } else {
-          putquick "PRIVMSG $nick :Use "cancel #" to remove the Pokemon in the #th slot."
+          putquick "PRIVMSG $nick :Use \"cancel #\" to remove the Pokemon in the #th slot."
         }
       }
       reorder* {
